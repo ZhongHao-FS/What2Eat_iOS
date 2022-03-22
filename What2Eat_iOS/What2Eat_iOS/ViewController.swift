73c501e309c79608bbb05e6b75d28169a1f01595
ZhongHao-FS <75271488+ZhongHao-FS@users.noreply.github.com>
2022-03-22 00:21:56 -0400

    Firebase installed

---
+++ What2Eat_iOS/What2Eat_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  What2Eat_iOS
+//
+//  Created by Hao Zhong on 3/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ What2Eat_iOS/What2Eat_iOSUITests/What2Eat_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  What2Eat_iOSUITestsLaunchTests.swift
+//  What2Eat_iOSUITests
+//
+//  Created by Hao Zhong on 3/21/22.
+//
+
+import XCTest
+
+class What2Eat_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
