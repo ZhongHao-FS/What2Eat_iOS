682b3594c2714ff55adf0b37f12b4d36d4b0d646
ZhongHao-FS <75271488+ZhongHao-FS@users.noreply.github.com>
2022-03-23 19:51:27 -0400

    Main search screen

---
+++ What2Eat_iOS/What2Eat_iOS/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  What2Eat_iOS
+//
+//  Created by Hao Zhong on 3/23/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var searchInput: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func locationTapped(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
